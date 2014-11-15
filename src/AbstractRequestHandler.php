908d9553c1b27141ef5ce86ab40fbdfe81995111
Tpt <thomaspt@hotmail.fr>
2014-11-15 12:29:34 +0100

    Adds basic implementation of RequestHandler
    Allows to adds easily methods with default comportement without breaking modules

---
+++ src/AbstractRequestHandler.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace PPP\Module;
+
+/**
+ * Basic implementation of requestHandler
+ *
+ * @licence MIT
+ * @author Thomas Pellissier Tanon
+ */
+abstract class AbstractRequestHandler implements RequestHandler {
+}
