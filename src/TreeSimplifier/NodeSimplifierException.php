7e24a113a8a2c89bab4c25d56c1ba6e971253f2e
Tpt <thomaspt@hotmail.fr>
2014-11-28 15:16:40 +0100

    Adds NodeSimplifierException

---
+++ src/TreeSimplifier/NodeSimplifierException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace PPP\Wikidata\SentenceTreeSimplifier;
+
+use RuntimeException;
+
+/**
+ * Exception thrown if things go wrong during the simplification.
+ *
+ * @licence GPLv2+
+ * @author Thomas Pellissier Tanon
+ */
+class NodeSimplifierException extends RuntimeException {
+}
