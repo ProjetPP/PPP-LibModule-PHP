02e4a9b2b6cac7a529a458a9af184fe8676f01ed
Tpt <thomaspt@hotmail.fr>
2014-12-08 09:58:24 +0100

    + FirstNodeSimplifier

---
+++ src/TreeSimplifier/FirstNodeSimplifier.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace PPP\Module\TreeSimplifier;
+
+use InvalidArgumentException;
+use PPP\DataModel\AbstractNode;
+use PPP\DataModel\ResourceListNode;
+use PPP\DataModel\FirstNode;
+
+/**
+ * @licence MIT
+ * @author Thomas Pellissier Tanon
+ */
+class FirstNodeSimplifier implements NodeSimplifier {
+
+	/**
+	 * @var NodeSimplifierFactory
+	 */
+	private $simplifierFactory;
+
+	/**
+	 * @param NodeSimplifierFactory $simplifierFactory
+	 */
+	public function __construct(NodeSimplifierFactory $simplifierFactory) {
+		$this->simplifierFactory = $simplifierFactory;
+	}
+
+	/**
+	 * @see NodeSimplifier::isSimplifierFor
+	 */
+	public function isSimplifierFor(AbstractNode $node) {
+		return $node instanceof FirstNode;
+	}
+
+	/**
+	 * @see NodeSimplifier::simplify
+	 * @param FirstNode $node
+	 */
+	public function simplify(AbstractNode $node) {
+		if(!$this->isSimplifierFor($node)) {
+			throw new InvalidArgumentException('FirstNodeSimplifier can only simplify FirstNode');
+		}
+
+		$nodeSimplifier = $this->simplifierFactory->newNodeSimplifier();
+		$operand = $nodeSimplifier->simplify($node->getOperand());
+
+		if($operand instanceof ResourceListNode && !$operand->isEmpty()) {
+			$resources = $operand->toArray();
+			return new ResourceListNode(array(reset($resources)));
+		} else {
+			return $operand;
+		}
+	}
+}
---
+++ tests/TreeSimplifier/FirstNodeSimplifierTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace PPP\Module\TreeSimplifier;
+
+use PPP\DataModel\IntersectionNode;
+use PPP\DataModel\ResourceListNode;
+use PPP\DataModel\StringResourceNode;
+use PPP\DataModel\FirstNode;
+
+/**
+ * @covers PPP\Module\TreeSimplifier\FirstNodeSimplifier
+ *
+ * @licence MIT
+ * @author Thomas Pellissier Tanon
+ */
+class FirstNodeSimplifierTest extends NodeSimplifierBaseTest {
+
+	protected function buildSimplifier() {
+		return new FirstNodeSimplifier(new NodeSimplifierFactory());
+	}
+
+	public function simplifiableProvider() {
+		return array(
+			array(
+				new FirstNode(new ResourceListNode(array()))
+			)
+		);
+	}
+
+	public function nonSimplifiableProvider() {
+		return array(
+			array(
+				new IntersectionNode(array())
+			)
+		);
+	}
+
+	public function simplificationProvider() {
+		return array(
+			array(
+				new ResourceListNode(array(
+					new StringResourceNode('foo')
+				)),
+				new FirstNode(new ResourceListNode(array(
+					new StringResourceNode('foo'),
+					new StringResourceNode('bar')
+				)))
+			),
+			array(
+				new ResourceListNode(array()),
+				new FirstNode(new ResourceListNode(array()))
+			)
+		);
+	}
+}
